35bbd24bb4af7b291c0cb9ca0bf08f4300de8060
Robin Caroff <robin.caroff@gmail.com>
2019-08-08 22:50:03 +0200

    👍 Uses a ViewModel to handle the presentation logic.

--- androidApp/src/main/java/xyz/mlumeau/kosmos/MainActivity.kt
+++ androidApp/src/main/java/xyz/mlumeau/kosmos/MainActivity.kt
@@ -1,41 +1,35 @@
 package xyz.mlumeau.kosmos
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Job
-import xyz.mlumeau.kosmos.kore.NasaAPIRemote
+import xyz.mlumeau.kosmos.kore.APOD
 import xyz.mlumeau.kosmos.kore.createApplicationScreenMessage
-import xyz.mlumeau.kosmos.kore.service.nasa.NasaApi
+import xyz.mlumeau.kosmos.viewmodel.APODViewModel
 
 class MainActivity : AppCompatActivity() {
-
-    private var job: Job? = null
-    private val nasaAPI: NasaApi = NasaAPIRemote()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title_tv.text = createApplicationScreenMessage()
 
-        job = nasaAPI.getAPOD{ apod ->
-            runOnUiThread {
-                if(apod.media_type == "image" && !apod.url.isNullOrEmpty()) {
-                    Picasso.get().load(apod.url).fit().centerCrop().into(apod_iv)
-                    title_tv.text = apod.title
-                    desc_tv.text = apod.explanation
-                }else{
-                    title_tv.text = "Sorry, no picture today :("
-                }
-                progress.visibility = View.GONE
-            }
-        }
+        val model = ViewModelProviders.of(this)[APODViewModel::class.java]
+        model.apod.observe(this, Observer { apod -> updateAPODData(apod) })
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        job?.cancel()
+    private fun updateAPODData(apod: APOD) {
+        if (apod.media_type == "image" && !apod.url.isNullOrEmpty()) {
+            Picasso.get().load(apod.url).fit().centerCrop().into(apod_iv)
+            title_tv.text = apod.title
+            desc_tv.text = apod.explanation
+        } else {
+            title_tv.text = "Sorry, no picture today :("
+        }
+        progress.visibility = View.GONE
     }
 }
