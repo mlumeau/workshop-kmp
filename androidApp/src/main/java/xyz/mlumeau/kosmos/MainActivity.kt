4548478b58e2743e264bbd3fb5534721df8d99fb
Robin Caroff <robin.caroff@gmail.com>
2019-08-08 13:59:57 +0200

    ⬆️ Updates dependencies and improves declaration.

--- androidApp/src/main/java/xyz/mlumeau/kosmos/MainActivity.kt
+++ androidApp/src/main/java/xyz/mlumeau/kosmos/MainActivity.kt
@@ -1,7 +1,7 @@
 package xyz.mlumeau.kosmos
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
