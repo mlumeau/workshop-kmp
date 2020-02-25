699577aba97e62a7a14e726685d6ab59843b4c91
Robin Caroff <robin.caroff@gmail.com>
2020-02-25 11:05:58 +0100

    ↩️ Merges branch 'step_six_getapodusecase' into step_five_architecture and removes exposed coroutines functions from the common kore interfaces to simplify the workshop.

--- androidApp/src/main/java/xyz/mlumeau/kosmos/views/MainActivity.kt
+++ androidApp/src/main/java/xyz/mlumeau/kosmos/views/MainActivity.kt
@@ -5,9 +5,9 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
 import xyz.mlumeau.kosmos.R
-import xyz.mlumeau.kosmos.kore.createApplicationScreenMessage
 import xyz.mlumeau.kosmos.kore.data.APODRepositoryRemote
 import xyz.mlumeau.kosmos.kore.data.APODRepositoryRemoteImpl
 import xyz.mlumeau.kosmos.kore.model.APOD
@@ -19,29 +19,31 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        title_tv.text = createApplicationScreenMessage()
 
         getAPOD()
     }
 
     private fun updateAPODData(apod: APOD) {
-        title_tv.text = apod.title
-        desc_tv.text = apod.explanation
-        if (apod.media_type == "image" && !apod.url.isNullOrEmpty()) {
-            Picasso.get().load(apod.url).fit().centerCrop().into(apod_iv)
-        } else {
-            apod_iv.visibility = View.GONE
+        runOnUiThread {
+            title_tv.text = apod.title
+            desc_tv.text = apod.explanation
+            if (apod.media_type == "image" && !apod.url.isNullOrEmpty()) {
+                Picasso.get().load(apod.url).fit().centerCrop().into(apod_iv)
+            } else {
+                apod_iv.visibility = View.GONE
+            }
+            progress.visibility = View.GONE
         }
-        progress.visibility = View.GONE
     }
 
     private fun getAPOD() {
-        launch(Dispatchers.IO) {
-            apodRepository.getAPOD()?.let { apod ->
-                withContext(Dispatchers.Main) {
-                    updateAPODData(apod)
-                }
-            }
-        }
+        apodRepository.getAPOD(
+            this::updateAPODData,
+            this::onAPODLoadingError
+        )
+    }
+
+    private fun onAPODLoadingError() {
+        // Handle the error
     }
 }
